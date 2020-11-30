bce7561a411d56df67e019f792dde040fb28905a
sobadrush <sobadrush@icloud.com>
2020-11-30 20:38:31 +0800

    initial commit

---
+++ src/main/java/com/example/demo/SpringEhcacheTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEhcacheTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEhcacheTestApplication.class, args);
+	}
+
+}
